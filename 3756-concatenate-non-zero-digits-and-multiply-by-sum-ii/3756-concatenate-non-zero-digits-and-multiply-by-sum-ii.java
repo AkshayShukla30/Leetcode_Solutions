b6478e90f68645c770e6e19b9cea3f14bf67f128
Akshay Shukla <akshayshukla466@gmail.com>
2026-07-08 08:54:00 +0530

    3756-concatenate-non-zero-digits-and-multiply-by-sum-ii

---
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+class Solution {
+    private static final int MOD = 1_000_000_007;
+
+    public int[] sumAndMultiply(String s, int[][] queries) {
+        int n = s.length();
+
+        ArrayList<Integer> posList = new ArrayList<>();
+        ArrayList<Integer> digitList = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int d = s.charAt(i) - '0';
+            if (d != 0) {
+                posList.add(i);
+                digitList.add(d);
+            }
+        }
+
+        int k = digitList.size();
+        int[] pos = new int[k];
+        int[] digit = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            pos[i] = posList.get(i);
+            digit[i] = digitList.get(i);
+        }
+
+        long[] pow10 = new long[k + 1];
+        pow10[0] = 1;
+        for (int i = 1; i <= k; i++) {
+            pow10[i] = (pow10[i - 1] * 10) % MOD;
+        }
+
+        long[] prefNum = new long[k + 1];
+        for (int i = 0; i < k; i++) {
+            prefNum[i + 1] = (prefNum[i] * 10 + digit[i]) % MOD;
+        }
+
+        long[] prefSum = new long[k + 1];
+        for (int i = 0; i < k; i++) {
+            prefSum[i + 1] = prefSum[i] + digit[i];
+        }
+
+        int[] ans = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int l = queries[i][0];
+            int r = queries[i][1];
+
+            int left = lowerBound(pos, l);
+            int right = upperBound(pos, r) - 1;
+
+            if (left > right) {
+                ans[i] = 0;
+                continue;
+            }
+
+            int len = right - left + 1;
+
+            long x = (prefNum[right + 1] - (prefNum[left] * pow10[len]) % MOD + MOD) % MOD;
+            long sum = prefSum[right + 1] - prefSum[left];
+
+            ans[i] = (int) ((x * (sum % MOD)) % MOD);
+        }
+
+        return ans;
+    }
+
+    private int lowerBound(int[] arr, int target) {
+        int l = 0, r = arr.length;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] < target)
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        return l;
+    }
+
+    private int upperBound(int[] arr, int target) {
+        int l = 0, r = arr.length;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] <= target)
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        return l;
+    }
+}
