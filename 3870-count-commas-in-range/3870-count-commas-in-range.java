07a309405fc7be79cd19dccb558a559db6783df3
Akshay Shukla <akshayshukla466@gmail.com>
2026-09-08 19:57:51 +0530

    3870-count-commas-in-range

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,11 +1,5 @@
 class Solution {
     public int countCommas(int n) {
-        int count = 0;
-
-        for (int i = 1000; i <= n; i++) {
-            count++;
-        }
-
-        return count;
+        return n >= 1000 ? n - 999 : 0;
     }
 }
